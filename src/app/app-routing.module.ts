087fb375e9fb62156a15468c9a2d67e697df7ccf
BertiniM <91059066+BertiniM@users.noreply.github.com>
2022-06-27 17:01:22 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ConnexionComponent} from './connexion/connexion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'connexion',
+    component: ConnexionComponent
+  },
+  { path: '',   redirectTo: '/connexion', pathMatch: 'full' },
+  { path: '**', component:  ConnexionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
+    CheckboxModule,
+    ButtonModule,
+    PanelModule,
+    AccordionModule,
     AppRoutingModule
   ],
   providers: [],
